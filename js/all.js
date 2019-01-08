6f769360be590a1fa687c1488ec63e2de9c7d2db
yuhow840522 <yuhow840522@gmail.com>
2019-01-08 17:48:07 +0800

    scrollTop btn
    add new function of scrollTop button

--- js/all.js
+++ js/all.js
@@ -1,19 +1,25 @@
 $(document).ready(function() {
-  var mySwiper = new Swiper ('.swiper-container', {
-    // 可選參數
-    loop: true,
+    var mySwiper = new Swiper('.swiper-container', {
+        // 可選參數
+        loop: true,
 
-    // 如果需要分頁設置
-    pagination: {
-      el: '.swiper-pagination',
-    },
+        // 如果需要分頁設置
+        pagination: {
+            el: '.swiper-pagination',
+        },
 
-    // 上一個/下一個 按鈕
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  })
+        // 上一個/下一個 按鈕
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+    // add scrollTop button
+    $('.btn-backtop').click(function(event) {
+        event.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
 
-  
 });
